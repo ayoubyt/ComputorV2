9d26ee54fc684ac3fe71ad4bb81c692b195a1229
Ayoub Aguert <aaguert@e1r13p5.1337.ma>
2020-12-14 14:09:57 +0100

    adding a test file

--- test/test_computorv2.py
+++ test/test_computorv2.py
@@ -1,19 +1,47 @@
 import pexpect as pe
 import re
 
-test_cases = ["555+555i =?", "[[1, 1]]"]
+#defigns
 
 propmpt = ">"
 
 ps = pe.spawn("python bin/computorv2.py")
 ps.expect(propmpt)
-ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
 
 
-
-for t in test_cases:
-    ps.sendline(t)
+def type_line(text):
+    ps.sendline(text)
     ps.expect("\r\n")
     ps.expect(propmpt)
-    user_res = ansi_escape.sub("", ps.before.decode()).strip()
-    print(ansi_escape.sub("", ps.before.decode()).strip())
+    res = ansi_escape.sub("", ps.before.decode()).strip()
+    return res
+
+
+def test_number_calculations():
+    test_cases = [
+        ("1 + 1 =?", "2"),
+        ("1 / 3 =?", f"{1 / 3:.6}"),
+        ("1 + 2 + 3 * 5 + 4 * (2 + 1 * 8 -(2*3*6-5*(4+1+2+9*6-5/4/2)))/6=?",
+         f"{1 + 2 + 3 * 5 + 4 * (2 + 1 * 8 - (2 * 3 * 6 - 5 * (4 + 1 + 2 + 9 * 6 - 5 / 4 / 2))) / 6:.6}")
+    ]
+
+    for test in test_cases:
+        result = type_line(test[0])
+        assert result == test[1]
+
+def test_number_weird_calculations():
+    test_cases = [
+        ("-2 =?", f"{-2}"),
+        ("2 * -1 + 1 =?" f"{2 * -1 + 1}"),
+        ("2 * 0-(4 * 2)" f"{2 * - (4 * 2)}"),
+       
+    ]
+
+    for test in test_cases:
+        result = type_line(test[0])
+        print(result, test)
+        # assert result == test[1]
+test_number_weird_calculations()
+
+ps.kill(0)
