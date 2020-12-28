c304cc2bc5c116269aeab7014aa382a728d0f4f7
ayoubyt <ayoub.aguert@gmail.com>
2020-12-28 13:00:20 +0100

    .

--- test/test_computorv2.py
+++ test/test_computorv2.py
@@ -27,15 +27,13 @@ def type_line(text):
 
 class TestNumCalc(unittest.TestCase):
 
-    def test_number_weird_calculations(self):
+    def test_number_simple_calculations(self):
         test_cases = [
             ("-2 =?", f"{-2}"),
             ("2 * -1 + 1 =?", f"{2 * -1 + 1}"),
-            ("2 * 0-(4 * 2)", f"{2 * 0-(4 * 2)}"),
             ("1 + 1 =?", "2"),
             ("1 / 3 =?", f"{1 / 3:.6}"),
-            ("1 + 2 + 3 * 5 + 4 * (2 + 1 * 8 -(2*3*6-5*(4+1+2+9*6-5/4/2)))/6=?",
-                f"{1 + 2 + 3 * 5 + 4 * (2 + 1 * 8 -(2*3*6-5*(4+1+2+9*6-5/4/2)))/6:.6}")
+            ("0.2 *0.2 * 10 / 5", f"{0.2 *0.2 * 10 / 5:.6}"),
 
         ]
 
@@ -44,20 +42,31 @@ class TestNumCalc(unittest.TestCase):
             print("testing if : ", "'", test[0], "'", " == ", test[1])
             self.assertEqual(result, test[1])
 
-# test_number_weird_calculations()
+    def test_number_weird_calculations(self):
+        test_cases = [
+            ("2 * 0-(4 * 2)", f"{2 * 0-(4 * 2)}"),
+            ("1 + 2 + 3 * 5 + 4 * (2 + 1 * 8 -(2*3*6-5*(4+1+2+9*6-5/4/2)))/6=?",
+                f"{1 + 2 + 3 * 5 + 4 * (2 + 1 * 8 -(2*3*6-5*(4+1+2+9*6-5/4/2)))/6:.6}")
+        ]
 
+        for test in test_cases:
+            result = type_line(test[0])
+            print("testing if : ", "'", test[0], "'", " == ", test[1])
+            self.assertEqual(result, test[1])
 
-# def test_number_weird_calculations():
-#     test_cases = [
-#         ("1 + 1 =?", "2"),
-#         ("1 / 3 =?", f"{1 / 3:.6}"),
-#         ("1 + 2 + 3 * 5 + 4 * (2 + 1 * 8 -(2*3*6-5*(4+1+2+9*6-5/4/2)))/6=?",
-#          f"{1 + 2 + 3 * 5 + 4 * (2 + 1 * 8 -(2*3*6-5*(4+1+2+9*6-5/4/2)))/6:.6}")
-#     ]
-#     for t in test_cases:
-#         assert str(eval_input(t[0])) == t[1]
+class TestAssign(unittest.TestCase):
 
-# print(program.eval_input("1 + 1"))
+    def test_number_simple_asignment(self):
+        test_cases = [
+           ("a = 1", "1"),
+           ("a + 1 =?", "2")
+        ]
+
+        for test in test_cases:
+            result = type_line(test[0])
+            print("testing if : ", "'", test[0], "'", " == ", test[1])
+            self.assertEqual(result, test[1])
+
 
 if __name__ == "__main__":
     unittest.main()
